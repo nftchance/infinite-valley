12c70861c92e5b38e757afb35661671a4249b488
nftchance <chance@utc24.io>
2023-12-05 02:49:32 -0600

    fix: super long cache

--- api/day/views.py
+++ api/day/views.py
@@ -1,6 +1,7 @@
 import base64
 
-from django.http import HttpResponse, FileResponse
+from django.core.exceptions import BadRequest
+from django.http import HttpResponse
 from django.views.decorators.cache import cache_page
 
 from .models import Day
@@ -12,20 +13,14 @@ def get_day(day=None):
 
     return Day.objects.filter(day__isnull=False).order_by('-id').first()
 
-@cache_page(60 * 10)
+
+@cache_page(60 * 60 * 24)
 def day(request, day=None):
     dayObj = get_day(day)
 
     if not dayObj:
-        return HttpResponse((
-            'What are you doing? Trying to look into the future as if you are '
-            'an oracle for those under your control. </br>The medium of this '
-            'day is still unknown and you trying to peer again will not '
-            'hasten its arrival. </br>You will have to wait like the rest '
-            'of us. </br></br>I still love you though.</br></br>'
-            '- <a target="_blank" href="https://chance.utc24.io">CHANCE</a>'
-        ))
-
+        raise BadRequest("Day not found")
+        
     image_data = base64.b64decode(dayObj.image_data)
 
     return HttpResponse(image_data, content_type='image/png')
